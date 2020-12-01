538b6dd544f2f7d1bf7b15f1d706aaaa1a09335d
Audrey Langlois <azpmaggie@gmail.com>
2020-12-01 09:01:21 -0500

    Code ecrit pour js, mais pas de npm, car un nouveau laptop

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let connexion = new MovieDB();
+    connexion.requeteDerniersFilm();
+});
+
+class MovieDB {
+    constructor() {
+        console.log('new MovieDB()');
+        this.appiKey = 'edfc0d8a17f0e7d7374973505bfaedc8';
+        this.lang = 'fr-CA';
+        this.baseURL = 'https://api.themoviedb.org/3/';
+        this.imgPath = 'https://image.tmdb.org/t/p/';
+        this.totaleFilm = 8;
+    }
+
+    requeteDerniersFilm() {
+        let requete = new XMLHttpRequest();
+        requete.addEventListener('loadend', this.retourRequeteDerniersFilm.bind(this));
+        requete.open('GET', this.baseURL + 'movie/now_playing?api_key=' + this.appiKey + '&language=' + this.lang + '&page=1')
+        requete.send();
+    }
+
+    retourRequeteDerniersFilm(event) {
+        console.log('ça marche');
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText).results;
+        console.log(data);
+        this.afficheDerniersFilm(data);
+    }
+
+    afficheDerniersFilm(data) {
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].title);
+            console.log(data[i].overview);
+        }
+    }
+}
