b49a4f94aa90fd6b4925a26aa976e85cb0b8ca81
Audrey Langlois <azpmaggie@gmail.com>
2020-12-01 09:02:19 -0500

    npm installer et git configurer pour continuer le projet

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let connexion = new MovieDB();
     connexion.requeteDerniersFilm();
 });
